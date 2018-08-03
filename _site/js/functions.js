5b12b093e7d0d2a98f6a5c2ac3b28cfe4d577935
BarboraKemezyte <aimenbenfika@gmail.com>
2018-08-03 22:46:09 +0100

    portfolio update
    redesign of portfolio

--- _site/js/functions.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-  //smoothScroll(2000);
-
-  AOS.init({
-    duration: 800,
-  });
-});
--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,28 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
-  //smoothScroll(2000);
 
-  AOS.init({
-    duration: 800,
+// Smooth scrolling
+  var scrollLink = $('.scroll');
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
   });
-});
+
+  //underline switching
+  $("a").click(function () {
+    $('table tr th a').removeClass('active');
+    $('table tr td a').removeClass('active');
+    $(this).toggleClass('active');
+ });
+
+
+ //sidenav toggle - mobile view
+ $('.sidenav-toggle').click(function(){
+
+   $('.sidenav-toggle img').toggleClass('rotate');
+   $('.sidenav').toggleClass('open');
+ })
+
+})
